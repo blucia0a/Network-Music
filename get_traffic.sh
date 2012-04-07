b8bb8e1306904071ceb9e287f1ceef928a57d1c3
Brandon Lucia <blucia@gmail.com>
2012-04-07 14:48:48 -0700

    friendly committing for good of all

--- get_traffic.sh
+++ get_traffic.sh
@@ -11,7 +11,7 @@
 # In that file, put your:
 #   -o wlan... argument.
 
-if [ "$#" -eq 1 ]; then
+if [ "$#" -eq 0 ]; then
   echo "Usage: get_traffic.sh [interface name (en1, etc)]"
   exit 1
 fi
@@ -22,7 +22,7 @@ if [ ! -f $NETWORK_MUSIC_WIFI_KEY_FILE ]; then
 fi
 
 #tshark -l -ien1 -o wlan.enable_decryption:TRUE -o wlan.wep_key1:wpa-psk:2dcc65f8d1f2fa08f6789b6533775bf19897bc6434987fc0a756734d82c4332a -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code
-#tshark -l -i$1 -o wlan.enable_decryption:TRUE `cat $NETWORK_MUSIC_WIFI_KEY_FILE` -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code
-tshark -l -ien1 -o wlan.enable_decryption:TRUE -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code -e icmp.mip.length
+tshark -l -i$1 -o wlan.enable_decryption:TRUE `cat $NETWORK_MUSIC_WIFI_KEY_FILE` -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code
+#tshark -l -ien1 -o wlan.enable_decryption:TRUE -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code -e icmp.mip.length
 #tshark -ien1 -o 'wlan.enable_decryption:TRUE' -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport 
 #tshark -ien1 -o 'wlan.enable_decryption:TRUE' -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e bittorrent.msg
