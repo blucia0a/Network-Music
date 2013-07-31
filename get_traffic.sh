3d29439b36fc7c57f63f1e9a8963f4fef7fb5b6e
Brandon Lucia <blucia0a@d-173-250-193-196.dhcp4.washington.edu>
2013-07-31 14:02:16 -0700

    committing a patched version of get_traffic.sh that avoids a hang when the network key file isn't detected.
    added some minor reconfiguration for the intro and for section 3 (spooky bells part) intro.rax3 and part2.rax3

--- get_traffic.sh
+++ get_traffic.sh
@@ -13,15 +13,21 @@
 
 if [ "$#" -eq 0 ]; then
   echo "Usage: get_traffic.sh [interface name (en1, etc)]"
-  exit 1
+  exit 
 fi
 
-if [ ! -f $NETWORK_MUSIC_WIFI_KEY_FILE ]; then
-  echo "There is no NETWORK_MUSIC_WIFI_KEY_FILE at $NETWORK_MUSIC_WIFI_KEY_FILE"
+if [ "${NETWORK_MUSIC_WIFI_KEY_FILE}" = "" ]; then
+  NETWORK_MUSIC_WIFI_KEY_FILE="${HOME}/.network-music/keyfile"
+  echo "Monitoring traffic on $1 with authentication in NETWORK_MUSIC_WIFI_KEY_FILE at $NETWORK_MUSIC_WIFI_KEY_FILE"
+fi
+
+if [ ! -f ${NETWORK_MUSIC_WIFI_KEY_FILE} ]; then
+  echo "$NETWORK_MUSIC_WIFI_KEY_FILE does not exist!"
   exit 1
 fi
 
 #tshark -l -ien1 -o wlan.enable_decryption:TRUE -o wlan.wep_key1:wpa-psk:2dcc65f8d1f2fa08f6789b6533775bf19897bc6434987fc0a756734d82c4332a -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code
+echo "Running \"tshark -l -i$1 -o wlan.enable_decryption:TRUE `cat $NETWORK_MUSIC_WIFI_KEY_FILE` -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code\""
 tshark -l -i$1 -o wlan.enable_decryption:TRUE `cat $NETWORK_MUSIC_WIFI_KEY_FILE` -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code
 #tshark -l -ien1 -o wlan.enable_decryption:TRUE -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e ip.dst -e tcp.srcport -e tcp.dstport -e icmp.code -e icmp.mip.length
 #tshark -ien1 -o 'wlan.enable_decryption:TRUE' -o "wlan.wep_key1:sv4j6xma7yz6je" -nn -Tfields -Eseparator="/t" -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport 
