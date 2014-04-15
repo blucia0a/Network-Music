c4da3ab3288f827e4c3ea181250599239306e2cb
David Balatero <dbalatero@gmail.com>
2014-04-14 21:10:39 -0700

    send script

--- send.sh
+++ send.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  for i in 41000 41001 41002 41003 41004 41005; do
+  for i in 41000 41001 41002 41003 41004 41005 41006 41007; do
     echo "balls" | nc $1 $i;
   done;
 
